b1ea7dff042b49a474b4d36aa4724839a5646fe5
Joe Mazzone <jmazzone@daviestech.org>
2020-10-02 08:58:09 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 1.6 Activity
+
+This activity has 3 questions.  
+
+See Activity Worksheet for information on what needs to be completed for each question.
---
+++ question3.py
@@ -0,0 +1,11 @@
+animal = input("Enter an animal: ")
+sound = input ("Enter a sound: ")
+
+e = "E"
+
+print ("Old Macdonald had a farm, " + e)
+print ("And on his farm he had a" + animal + "," + e)
+print ("With a " + animal + "-" + animal + " here and a" + sound + "-" + sound + " there")
+print ("Here a "+ sound+ " there a " +sound)
+print ("Everywhere a" + sound + "-" + animal )
+print ("Old Macdonald had a farm," + e)
